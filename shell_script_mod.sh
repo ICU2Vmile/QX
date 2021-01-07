0f82d3a9182b286f5c536f6ebc2ef80135f435a4
ICU2Vmile <47917707+ICU2Vmile@users.noreply.github.com>
2021-01-07 17:28:10 +0800

    Create shell_script_mod.sh

---
+++ shell_script_mod.sh
@@ -0,0 +1,14 @@
+##使用自定义shell下载并配置中青看点极速版
+wget -O /scripts/youth.js https://raw.githubusercontent.com/Sunert/Scripts/master/Task/youth.js
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "11 8 * * * node /scripts/youth.js |ts >> /scripts/logs/youth.log 2>&1" >> /scripts/docker/merged_list_file.sh
+
+##使用自定义shell下载并配置中青看点自动阅读
+wget -O /scripts/Youth_Read.js https://raw.githubusercontent.com/Sunert/Scripts/master/Task/Youth_Read.js
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "15 */2 * * * node /scripts/Youth_Read.js |ts >> /scripts/logs/Youth_Read.log 2>&1" >> /scripts/docker/merged_list_file.sh
+
+##使用自定义shell下载并配置中青看点浏览赚
+wget -O /scripts/youth_gain.js https://raw.githubusercontent.com/Sunert/Scripts/master/Task/youth_gain.js
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "5 10 * * * node /scripts/youth_gain.js |ts >> /scripts/logs/youth_gain.log 2>&1" >> /scripts/docker/merged_list_file.sh
